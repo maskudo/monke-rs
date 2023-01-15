b3cf15a99f43dbb8ee2156ebe244dd1fafe2d590
maskudo <manujkarki101@gmail.com>
2023-01-15 18:43:27 +0545

    add dependency and repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use linefeed::{Interface, ReadResult};
+use monke_rs::lexer::token::Token;
+use monke_rs::lexer::Lexer;
+
+const PROMPT: &str = ">>";
 fn main() {
-    println!("Hello, world!");
+    let reader = Interface::new(PROMPT).unwrap();
+    reader.set_prompt(format!("{}", PROMPT).as_ref()).unwrap();
+
+    while let ReadResult::Input(input) = reader.read_line().unwrap() {
+        if input.eq("exit") {
+            break;
+        }
+        let mut lex = Lexer::new(input);
+        loop {
+            let token = lex.next_token();
+            if token == Token::EOF {
+                break;
+            }
+            println!("{:?}", token);
+        }
+    }
 }
