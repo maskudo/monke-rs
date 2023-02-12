8ad4ba528752e67ce8aebdd4579ac1c7d7d1deac
maskudo <manujkarki101@gmail.com>
2023-02-12 22:18:21 +0545

    display parsed input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use linefeed::{Interface, ReadResult};
 use monke_rs::lexer::token::Token;
 use monke_rs::lexer::Lexer;
+use monke_rs::parser::Parser;
 
 const PROMPT: &str = ">>";
 fn main() {
@@ -11,13 +12,16 @@ fn main() {
         if input.eq("exit") {
             break;
         }
-        let mut lex = Lexer::new(input);
-        loop {
-            let token = lex.next_token();
-            if token == Token::EOF {
-                break;
+        let mut lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program = parser.parse_program();
+        if parser.errors().len() != 0 {
+            for error in parser.errors() {
+                eprintln!("{}", error);
             }
-            println!("{:?}", token);
+        } else {
+            println!("{program}");
         }
     }
 }
