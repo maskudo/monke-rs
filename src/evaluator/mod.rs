a2e61f210ddbf5e243b7b35bd28b78fbe8f89dba
maskudo <manujkarki101@gmail.com>
2023-02-17 18:47:00 +0545

    evaluate more literals and prefix expr

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,4 +1,4 @@
-use crate::parser::ast::{Expr, Literal, Program, Stmt};
+use crate::parser::ast::{self, Expr, Literal, Program, Stmt};
 
 use self::object::Object;
 
@@ -13,7 +13,6 @@ impl Evaluator {
         for statement in program.statements {
             match self.eval_stmt(statement) {
                 obj => result = obj,
-                _ => return None,
             };
         }
         result
@@ -29,14 +28,33 @@ impl Evaluator {
     fn eval_expr(&mut self, expr: Expr) -> Option<Object> {
         match expr {
             Expr::Literal(literal) => self.eval_literal(literal),
+            Expr::Prefix(prefix, expr) => self.eval_prefix(prefix, *expr),
             _ => None,
+        }
+    }
+
+    fn eval_prefix(&mut self, prefix: ast::Prefix, expr: Expr) -> Option<Object> {
+        match prefix {
+            ast::Prefix::Not => self.eval_expr(expr).and_then(|value| match value {
+                Object::Bool(bool) => Some(Object::Bool(!bool)),
+                _ => return None,
+            }),
+            ast::Prefix::Plus => self.eval_expr(expr).and_then(|obj| match obj {
+                Object::Int(int) => Some(Object::Int(int)),
+                _ => return None,
+            }),
+            ast::Prefix::Minus => self.eval_expr(expr).and_then(|obj| match obj {
+                Object::Int(int) => Some(Object::Int(-int)),
+                _ => return None,
+            }),
         }
     }
 
     fn eval_literal(&mut self, literal: Literal) -> Option<Object> {
         match literal {
             Literal::Int(value) => Some(Object::Int(value)),
-            _ => None,
+            Literal::String(value) => Some(Object::String(value)),
+            Literal::Bool(value) => Some(Object::Bool(value)),
         }
     }
 }
@@ -45,10 +63,7 @@ impl Evaluator {
 mod test {
     use std::vec;
 
-    use crate::{
-        lexer::{self, Lexer},
-        parser::{ast::Program, Parser},
-    };
+    use crate::{lexer::Lexer, parser::Parser};
 
     use super::{object::Object, Evaluator};
 
@@ -65,6 +80,33 @@ mod test {
         let tests = vec![
             ("5", Some(Object::Int(5))),
             ("10", Some(Object::Int(10))),
+            (("-5"), Some(Object::Int(-5))),
+            (("+5"), Some(Object::Int(5))),
+        ];
+        for (input, expect) in tests {
+            assert_eq!(expect, eval(input));
+        }
+    }
+
+    #[test]
+    fn test_eval_bool_expr() {
+        let tests = vec![
+            ("true", Some(Object::Bool(true))),
+            ("false", Some(Object::Bool(false))),
+        ];
+        for (input, expect) in tests {
+            assert_eq!(expect, eval(input));
+        }
+    }
+
+    #[test]
+    fn test_eval_bang_operator() {
+        //no truthy and falsey values
+        let tests = vec![
+            ("!true", Some(Object::Bool(false))),
+            ("!false", Some(Object::Bool(true))),
+            ("!!false", Some(Object::Bool(false))),
+            ("!!true", Some(Object::Bool(true))),
         ];
         for (input, expect) in tests {
             assert_eq!(expect, eval(input));
