322fbde084dcbe68437937e1424f3dd454e122a0
maskudo <manujkarki101@gmail.com>
2023-02-18 14:21:30 +0545

    evaluate infix expr

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,4 +1,4 @@
-use crate::parser::ast::{self, Expr, Literal, Program, Stmt};
+use crate::parser::ast::{self, Expr, Infix, Literal, Program, Stmt};
 
 use self::object::Object;
 
@@ -29,6 +29,7 @@ impl Evaluator {
         match expr {
             Expr::Literal(literal) => self.eval_literal(literal),
             Expr::Prefix(prefix, expr) => self.eval_prefix(prefix, *expr),
+            Expr::Infix(left, infix, right) => self.eval_infix(*left, infix, *right),
             _ => None,
         }
     }
@@ -47,6 +48,60 @@ impl Evaluator {
                 Object::Int(int) => Some(Object::Int(-int)),
                 _ => return None,
             }),
+        }
+    }
+
+    fn eval_infix(&mut self, left: Expr, infix: ast::Infix, right: Expr) -> Option<Object> {
+        match self.eval_expr(left)? {
+            Object::Int(left) => match self.eval_expr(right)? {
+                Object::Int(right) => Some(self.eval_infix_int_expr(left, infix, right)),
+                _ => return None,
+            },
+            Object::Bool(left) => match self.eval_expr(right)? {
+                Object::Bool(right) => self.eval_infix_bool_expr(left, infix, right),
+                _ => return None,
+            },
+            Object::String(left) => match self.eval_expr(right)? {
+                Object::String(right) => self.eval_infix_string_expr(left, infix, right),
+                _ => return None,
+            },
+            _ => None,
+        }
+    }
+
+    fn eval_infix_int_expr(&mut self, left: i64, infix: Infix, right: i64) -> Object {
+        //possible panic when value crosses range of i64
+        match infix {
+            Infix::Plus => Object::Int(left + right),
+            Infix::Minus => Object::Int(left - right),
+            Infix::Multiply => Object::Int(left * right),
+            Infix::Divide => Object::Int(left / right),
+            Infix::LessThan => Object::Bool(left < right),
+            Infix::GreaterThan => Object::Bool(left > right),
+            Infix::LessEqual => Object::Bool(left <= right),
+            Infix::GreaterEqual => Object::Bool(left >= right),
+            Infix::Equal => Object::Bool(left == right),
+            Infix::NotEqual => Object::Bool(left != right),
+        }
+    }
+
+    fn eval_infix_bool_expr(&mut self, left: bool, infix: Infix, right: bool) -> Option<Object> {
+        match infix {
+            Infix::Equal => Some(Object::Bool(left == right)),
+            Infix::NotEqual => Some(Object::Bool(left != right)),
+            _ => None,
+        }
+    }
+
+    fn eval_infix_string_expr(
+        &mut self,
+        left: String,
+        infix: Infix,
+        right: String,
+    ) -> Option<Object> {
+        match infix {
+            Infix::Plus => Some(Object::String(format!("{}{}", left, right))),
+            _ => None,
         }
     }
 
@@ -82,6 +137,27 @@ mod test {
             ("10", Some(Object::Int(10))),
             (("-5"), Some(Object::Int(-5))),
             (("+5"), Some(Object::Int(5))),
+            (("5 + 5 + 5 -10"), Some(Object::Int(5))),
+            (("5 + 5 + 5 + -10"), Some(Object::Int(5))),
+            (("5 * 5 / 5"), Some(Object::Int(5))),
+            (("(5 + 5) / 5"), Some(Object::Int(2))),
+        ];
+        for (input, expect) in tests {
+            assert_eq!(expect, eval(input));
+        }
+    }
+
+    #[test]
+    fn test_eval_string_expr() {
+        let tests = vec![
+            (
+                "\"hello world!\";",
+                Some(Object::String(String::from("hello world!"))),
+            ),
+            (
+                "\"hello \" + \"world!\";",
+                Some(Object::String(String::from("hello world!"))),
+            ),
         ];
         for (input, expect) in tests {
             assert_eq!(expect, eval(input));
@@ -93,6 +169,13 @@ mod test {
         let tests = vec![
             ("true", Some(Object::Bool(true))),
             ("false", Some(Object::Bool(false))),
+            ("false==false", Some(Object::Bool(true))),
+            ("false==true", Some(Object::Bool(false))),
+            ("false!=true", Some(Object::Bool(true))),
+            ("false!=false", Some(Object::Bool(false))),
+            (("(5 > 5)"), Some(Object::Bool(false))),
+            (("(5 >= 5)"), Some(Object::Bool(true))),
+            (("(5 - 5) > 5"), Some(Object::Bool(false))),
         ];
         for (input, expect) in tests {
             assert_eq!(expect, eval(input));
